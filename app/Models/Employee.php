f99317305357aff8b6b56fa00efe25ce7d6f3021
hazemelwaleed45 <hazemelwaleed45@gmail.com>
2024-11-02 00:05:51 +0200

    create Database Models and Migrations for Employees and departments

---
+++ app/Models/Employee.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'hire_date',
+        'salary',
+        'department_id'
+    ];
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+}
